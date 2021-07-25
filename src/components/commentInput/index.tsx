670fe038dfdd54412ae6b7308ac0617afb1e5da7
Manuel Rodriguez <marg.2197@gmail.com>
2021-07-25 16:10:42 -0500

    Hiding stories scrollbar in firefox

--- src/components/commentInput/index.tsx
+++ src/components/commentInput/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSmileBeam } from '@fortawesome/free-regular-svg-icons'
 import EmojiPicker, { IEmojiData } from 'emoji-picker-react'

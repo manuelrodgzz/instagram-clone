43790c23fbc4a9726582d6ce3ace5687e22bfc59
Manuel Alejandro Rodriguez Gonzalez <manuelalejandrorodriguezgonzalez@Manuels-Mac-mini.local>
2023-08-30 15:29:17 -0600

    Add main tag and update page title

--- src/components/posts/index.tsx
+++ src/components/posts/index.tsx
@@ -10,13 +10,13 @@ interface PostsProps {
 const Posts = ({users}: PostsProps) => {
 
     return (
-        <div className='posts'>
+        <main className='posts'>
             {users.map(user => (
                 user.posts && user.posts.length > 0 && user.posts.map(post => (
                     <Post key={user.user + String(post.likes)} post={post} user={user}/>
                 ))
             ))}
-        </div>
+        </main>
     )
 }
 
